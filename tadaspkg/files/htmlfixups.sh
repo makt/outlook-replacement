4393ae3ef981504afb6ca1ebdd29fc7fd2b2aa4b
Tadas Slotkus <makt@slotkus.lt>
2017-01-19 17:45:04 +0200

    iconv - omit invalid characters

--- tadaspkg/files/htmlfixups.sh
+++ tadaspkg/files/htmlfixups.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-iconv -f $1 -t utf8 |
+iconv -c -f $1 -t utf8 |
 perl -p -0777 -e '
  s,</p>\n*<p dir="ltr">,<br>,g;
+ s,</p>\n*<p class[^>]*>,<br>,g;
  s,<o:p>.*?</o:p>,,g' |
 sed -r '
- s,</p><p class=.?MsoPlainText.?>,<br>,g
  s,<div style=.border:none;border-top:solid #B5C4DF[^>]*><p class=.?MsoNormal.?><b><span [^>]*>,<div><p><b><span>_____________________________________________<br>,g
  s/(\xc2\xa0|&nbsp;) _{79}([^_])/  ________________________________________________\2/g'
